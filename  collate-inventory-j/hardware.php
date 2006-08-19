f6c37da181272a06c2d9f616718947b244f31173
jakepaulus <jakepaulus@c5412916-051b-0410-bfd6-8b17d4b8e5af>
2006-08-19 20:11:31 +0000

    Removed file/folder

---  collate-inventory-j/hardware.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('header.php');
-
-echo "<div id=\"main\">";
-
-echo "</div>";
-
-require_once('footer.php');
-?>
---  collate-inventory-j/software.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('header.php');
-?>
-<div id="main">
-</div>
-<?php
-require_once('footer.php');
-?>
